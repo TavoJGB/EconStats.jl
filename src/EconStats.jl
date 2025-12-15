81c0d148675a150012d8260cf955ad5bae2cd9b9
TavoJGB <116122159+TavoJGB@users.noreply.github.com>
2025-12-15 23:14:59 +0100

    Basic functions

--- src/EconStats.jl
+++ src/EconStats.jl
@@ -5,7 +5,7 @@ module EconStats
     # External dependencies
     using StatsBase         # weighted mean etc.
     using StatsBase: dot
-        export mean, std, median, dot
+        export mean, std, median, dot, quantile
 
     # Package dependencies
     include(joinpath(BASE_FOLDER, "src", "dep", "stats.jl"))
