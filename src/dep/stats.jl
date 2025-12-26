f8aede36b82f7a231c2cd7c8d31a445822bf3362
TavoJGB <116122159+TavoJGB@users.noreply.github.com>
2025-12-26 16:25:59 +0100

    weighted_sum

--- src/dep/stats.jl
+++ src/dep/stats.jl
@@ -1,7 +1,14 @@
+function weighted_sum_nonmissing(v::AbstractVector, weights::AbstractVector)
+    keep = @. !(ismissing(v) | ismissing(weights) | isnan(v) | isnan(weights))
+    dot(v[keep], weights[keep])
+end
+weighted_sum(v::AbstractVector, weights::AbstractVector; skipmissing::Bool=false) = skipmissing ? weighted_sum_nonmissing(v, weights) : dot(v, weights)
+
 function weighted_mean_nomissing(v::AbstractVector, weights::AbstractVector)
     keep = @. !(ismissing(v) | ismissing(weights) | isnan(v) | isnan(weights))
     dot(v[keep], weights[keep]) / sum(weights[keep])
 end
 weighted_mean(v::AbstractVector, weights::AbstractVector; skipmissing::Bool=false) = skipmissing ? weighted_mean_nomissing(v, weights) : dot(v, weights) / sum(weights)
+
 get_share(v::AbstractVector, condition::Function, weights::AbstractVector; kwargs...) = weighted_mean(condition.(v), weights; kwargs...)
 get_share(v::AbstractVector, condition, weights::AbstractVector; kwargs...) = get_share(v, x -> x == condition, weights; kwargs...)
