6f6557e669c74c81cee0510be6e31b91f224bc29
Gustavo Julio García Bernal <116122159+TavoJGB@users.noreply.github.com>
2026-09-20 12:31:18 +0200

    missing as zeros

--- src/dep/stats.jl
+++ src/dep/stats.jl
@@ -1,12 +1,33 @@
 #==========================================================================
+    DEALING WITH MISSING VALUES
+==========================================================================#
+
+function _missing_as_zeros(v::AbstractVector, func::Function, args...)
+    v_clean = @. ifelse(ismissing(v) | isnan(v), 0, v)
+    return func(v_clean, args...)
+end
+
+
+#==========================================================================
     WEIGHTED SUM
 ==========================================================================#
+_weighted_sum(v::AbstractVector, weights::AbstractVector) = dot(v, weights)
 
 function weighted_sum_nonmissing(v::AbstractVector, weights::AbstractVector)
     keep = @. !(ismissing(v) | ismissing(weights) | isnan(v) | isnan(weights))
-    dot(v[keep], weights[keep])
+    return _weighted_sum(v[keep], weights[keep])
 end
-weighted_sum(v::AbstractVector, weights::AbstractVector; skipmissing::Bool=false) = skipmissing ? weighted_sum_nonmissing(v, weights) : dot(v, weights)
+function weighted_sum(
+    v::AbstractVector, weights::AbstractVector;
+    skipmissing::Bool=false, missing_as_zeros::Bool=false)
+    if skipmissing
+        return weighted_sum_nonmissing(v, weights)
+    elseif missing_as_zeros
+        return _missing_as_zeros(v, _weighted_sum, weights)
+    else
+        return _weighted_sum(v, weights)
+    end
+end
 
 
 
@@ -14,11 +35,20 @@ weighted_sum(v::AbstractVector, weights::AbstractVector; skipmissing::Bool=false
     WEIGHTED AVERAGE
 ==========================================================================#
 
+_weighted_mean(v::AbstractVector, weights::AbstractVector) = dot(v, weights) / sum(weights)
 function weighted_mean_nomissing(v::AbstractVector, weights::AbstractVector)
     keep = @. !(ismissing(v) | ismissing(weights) | isnan(v) | isnan(weights))
-    return dot(v[keep], weights[keep]) / sum(weights[keep])
+    return _weighted_mean(v[keep], weights[keep])
 end
-weighted_mean(v::AbstractVector, weights::AbstractVector; skipmissing::Bool=false) = skipmissing ? weighted_mean_nomissing(v, weights) : dot(v, weights) / sum(weights)
+function weighted_mean(v::AbstractVector, weights::AbstractVector; skipmissing::Bool=false, missing_as_zeros::Bool=false)
+    if skipmissing
+        return weighted_mean_nomissing(v, weights)
+    elseif missing_as_zeros
+        return _missing_as_zeros(v, _weighted_mean, weights)
+    else
+        return _weighted_mean(v, weights)
+    end
+end
 
 
 
@@ -26,21 +56,8 @@ weighted_mean(v::AbstractVector, weights::AbstractVector; skipmissing::Bool=fals
     WEIGHTED MEDIAN
 ==========================================================================#
 
-function weighted_median_nomissing(v::AbstractVector, weights::AbstractVector)
-    keep = @. !(ismissing(v) | ismissing(weights) | isnan(v) | isnan(weights))
-    x = v[keep]
-    w = weights[keep]
-    isempty(x) && return missing
-    sorted_indices = sortperm(x)
-    x_sorted = x[sorted_indices]
-    w_sorted = w[sorted_indices]
-    cum_weights = cumsum(w_sorted)
-    half_total_weight = sum(w_sorted) / 2
-    median_index = findfirst(cum_weights .>= half_total_weight)
-    return x_sorted[median_index]
-end
-function weighted_median(v::AbstractVector, weights::AbstractVector; skipmissing::Bool=false)
-    skipmissing && return weighted_median_nomissing(v, weights)
+function _weighted_median(v::AbstractVector, weights::AbstractVector)
+    isempty(v) && return missing
     sorted_indices = sortperm(v)
     x_sorted = v[sorted_indices]
     w_sorted = weights[sorted_indices]
@@ -49,6 +66,19 @@ function weighted_median(v::AbstractVector, weights::AbstractVector; skipmissing
     median_index = findfirst(cum_weights .>= half_total_weight)
     return x_sorted[median_index]
 end
+function weighted_median_nomissing(v::AbstractVector, weights::AbstractVector)
+    keep = @. !(ismissing(v) | ismissing(weights) | isnan(v) | isnan(weights))
+    return _weighted_median(v[keep], weights[keep])
+end
+function weighted_median(v::AbstractVector, weights::AbstractVector; skipmissing::Bool=false, missing_as_zeros::Bool=false)
+    if skipmissing
+        return weighted_median_nomissing(v, weights)
+    elseif missing_as_zeros
+        return _missing_as_zeros(v, _weighted_median, weights)
+    else
+        return _weighted_median(v, weights)
+    end
+end
 
 
 
@@ -56,21 +86,23 @@ end
     WEIGHTED STANDARD DEVIATION
 ==========================================================================#
 
+function _weighted_std(v::AbstractVector, weights::AbstractVector)
+    isempty(v) && return missing
+    μ = _weighted_mean(v, weights)
+    return sqrt(dot(weights, (v .- μ).^2) / sum(weights))
+end
+
 function weighted_std_nomissing(v::AbstractVector, weights::AbstractVector)
     keep = @. !(ismissing(v) | ismissing(weights) | isnan(v) | isnan(weights))
-    x = v[keep]
-    w = weights[keep]
-    isempty(x) && return missing
-    μ = weighted_mean(x, w)
-    return sqrt(dot(w, (x .- μ).^2) / sum(w))
+    return _weighted_std(v[keep], weights[keep])
 end
-function weighted_std(v::AbstractVector, weights::AbstractVector; skipmissing::Bool=false)
-    isempty(v) && return missing
+function weighted_std(v::AbstractVector, weights::AbstractVector; skipmissing::Bool=false, missing_as_zeros::Bool=false)
     if skipmissing
         return weighted_std_nomissing(v, weights)
+    elseif missing_as_zeros
+        return _missing_as_zeros(v, _weighted_std, weights)
     else
-        μ = weighted_mean(v, weights)
-        return sqrt(dot(weights, (v .- μ).^2) / sum(weights))
+        return _weighted_std(v, weights)
     end
 end
 
